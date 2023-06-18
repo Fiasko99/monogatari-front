1e9d8d8722c4b9f2b55019070bc0a13e01d34531
SnakeRuse <snakeice2000@mail.ru>
2023-06-18 17:59:24 +0300

    add input ui kit

---
+++ src/ui-kit/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <input 
+    :type="type" 
+    :id="id" 
+    :name="name" 
+    @input="$emit('update:modelValue', $event.target.value)"
+    :required="required">
+</template>
+
+<script>
+export default {
+  name: 'InputUiKit',
+  props: {
+    id: String,
+    name: String,
+    type: {
+      type: String,
+      default: () => 'text'
+    },
+    modelValue: String,
+    required: Boolean,
+  },
+  emits: ['update:modelValue'],
+}
+</script>
+
+<style scoped>
+
+</style>
