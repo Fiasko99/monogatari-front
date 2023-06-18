390987e08484b6de3e0d5bb863fadd8c9285f85c
SnakeRuse <snakeice2000@mail.ru>
2023-06-18 18:01:46 +0300

    update main page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Header>
+    <template #navbar>
+      <Navbar />
+    </template>
+    <template #show-map-btn>
+      <Button @click="$store.commit('turnShowInteractiveMap')">Карта</Button>
+    </template>
+    <template #user-menu>
+      <UserMenu></UserMenu>
+    </template>
+  </Header>
+  <div class="app-main">
+    <router-view/>
+  </div>
 </template>
 
+<script>
+import UserMenu from "@/components/user-menu";
+import Header from "@/components/header";
+import Navbar from "@/components/navbar";
+import Button from "@/ui-kit/Button.vue";
+
+export default {
+  components: {
+    Header,
+    Navbar,
+    Button,
+    UserMenu,
+  },
+  async mounted() {
+    const token = localStorage.getItem(process.env.VUE_APP_TOKEN_NAME);
+    if (token) {
+      this.$store.commit('changeSignStatus', true)
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
-
-nav {
-  padding: 30px;
+body {
+  margin: 0;
+  padding: 0; 
+  background: lightcyan; 
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.app-main {
+  margin: 12px 0;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+@media screen and (max-width: 640px) {
+  .app-main {
+    margin: 12px 0;
+  }
+}
+@media screen and (min-width: 641px) and (max-width: 1440px) {
+  .app-main {
+    margin: 12px 5%;
+  }
+}
+@media screen and (min-width: 1441px) and (max-width: 2560px) {
+  .app-main {
+    margin: 12px 10%;
+  }
+}
+@media screen and (min-width: 2561px) {
+  .app-main {
+    margin: 12px 15%;
+  }
 }
 </style>
