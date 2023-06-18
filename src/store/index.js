7779114a3adf8bf7c96e4578bc252ac602a043c7
SnakeRuse <snakeice2000@mail.ru>
2023-06-18 17:58:57 +0300

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,39 @@
+import Fetcher from '@/Fetcher';
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    signStatus: false,
+    showInteractiveMap: false,
   },
   getters: {
+    signStatus({ signStatus }) {
+      return signStatus;
+    },
+    showInteractiveMap({ showInteractiveMap }) {
+      return showInteractiveMap;
+    },
   },
   mutations: {
+    changeSignStatus(state, value) {
+      state.signStatus = value;
+    },
+    turnShowInteractiveMap(state) {
+      state.showInteractiveMap = !state.showInteractiveMap;
+    },
   },
   actions: {
+    async signin({ commit }, payload) {
+      const res = await Fetcher.post(
+        '/user/signin',
+        payload
+      ).catch(err => console.log(err));
+      if (res) {
+        localStorage.setItem(process.env.VUE_APP_TOKEN_NAME, res.accessToken);
+        Fetcher.setToken();
+        commit('changeSignStatus', true);
+      }
+    },
   },
   modules: {
   }
