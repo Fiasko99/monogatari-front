dd4aed2663d98ffda9fe7b4df9c3e189ae8a7704
SnakeRuse <snakeice2000@mail.ru>
2023-06-18 17:58:51 +0300

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/Home.vue')
-  }
-]
+    component: () => import('../views/home')
+  },
+  {
+    path: '/location/:name',
+    name: 'location',
+    component: () => import('../views/location')
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('../views/auth')
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
