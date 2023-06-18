c2fdad59ba914fcaccd458c60a3c4bbf6d934fc4
SnakeRuse <snakeice2000@mail.ru>
2023-06-18 17:57:06 +0300

    add http client

--- src/Fetcher.js
+++ src/Fetcher.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 const config = {
   withCredentials: true,
   baseURL: process.env.VUE_APP_API_URL
@@ -9,40 +8,37 @@ class Http {
 
   constructor(queryTool) {
     this._http = queryTool.create(config)
-    this.#createConfig()
+    this.createConfig()
     this.setToken()
   }
 
-  async #reqWrapper(req, ...reqArgs) {
+  async reqWrapper(req, ...reqArgs) {
     const res = await req(...reqArgs)
-    .catch(async err => {
-      if (err?.response?.status == 402) {
-        const res = await this.get('/refresh')
-        this.removeToken()
-        localStorage.setItem(
-          process.env.VUE_APP_TOKEN_NAME, res.data.accessToken)
-        this.setToken()
-        const reqData = await req(...reqArgs)
-        return reqData
-      }
-    })
-    return res
+      .catch(async err => {
+        if (err.status == 402) {
+          await baseRefreshReq();
+          const reqData = await req(...reqArgs);
+          return reqData;
+        }
+        return Promise.reject(err);
+      })
+    return res;
   }
 
   async get(url) {
-    return await this.#reqWrapper(this._http.get, url)
+    return await this.reqWrapper(this._http.get, url)
   }
 
   async post(url, body, opt={}) {
-    return await this.#reqWrapper(this._http.post, url, body, opt)
+    return await this.reqWrapper(this._http.post, url, body, opt)
   }
 
   async put(url, body) {
-    return await this.#reqWrapper(this._http.put, url, body)
+    return await this.reqWrapper(this._http.put, url, body)
   }
 
   async delete(url) {
-    return await this.#reqWrapper(this._http.delete, url)
+    return await this.reqWrapper(this._http.delete, url)
   }
 
   setToken() {
@@ -56,12 +52,25 @@ class Http {
     this._http.defaults.headers.common["Authorization"] = null;
   }
 
-  #createConfig() {
+  async baseRefreshReq() {
+    const res = await this.get('/user/refresh')
+    .catch(err => console.log(err))
+    res && this.rebootToken(res.accessToken);
+  }
+
+  rebootToken(token) {
+    this.removeToken()
+    localStorage.setItem(
+      process.env.VUE_APP_TOKEN_NAME, 
+      token
+    )
+    this.setToken()
+  }
+
+  createConfig() {
     this._http.interceptors.response.use(
-      response => response,
-      (error) => {
-        return Promise.reject(error)
-      }
+      res => res.data,
+      err => Promise.reject(err.response)
     );
   }
 }
