a2831987d01faa709cdcf43c0ee9a7e010f9edb7
SnakeRuse <snakeice2000@mail.ru>
2023-06-18 17:59:34 +0300

    add label ui kit

---
+++ src/ui-kit/Label.vue
@@ -0,0 +1,18 @@
+<template>
+  <label :for="forId">
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'LabelUiKit',
+  props: {
+    forId: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
